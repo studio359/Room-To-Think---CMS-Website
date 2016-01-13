a4d9260071b04161a8450258ac1b07ca2034b34f
Dominic Danson <dominic@studio359.co.uk>
2016-01-13 19:31:27 +0000

    Homepage Item Template
    Created homepage Item.php template. Will be used for large homepage
    background image and welcome text.
    
    Module to follow beneath for latest/featured stories.

---
+++ Watchtower-Template/html/com_k2/home/item.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @version		2.6.x
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+?>
+<?php if($this->item->params->get('itemImage') && !empty($this->item->image)): ?>
+<section class="big-wrapper" style="background-image:url(<?php echo $this->item->imageLarge; ?>);">
+<?php else: ?>
+<section class="big-wrapper">
+<?php endif; ?>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <?php if(!empty($this->item->fulltext)): ?>
+        <?php if($this->item->params->get('itemIntroText')): ?>
+          <p class="lead-in"><?php echo $this->item->introtext; ?></p>
+        <?php endif; ?>
+        <?php if($this->item->params->get('itemFullText')): ?>
+          <p><?php echo $this->item->fulltext; ?></p>
+        <?php endif; ?>
+        <?php else: ?>
+          <p class="lead-in"><?php echo $this->item->introtext; ?></p>
+        <?php endif; ?>
+
+        <?php echo $this->item->event->AfterDisplayContent; ?>
+
+        <?php echo $this->item->event->K2AfterDisplayContent; ?>
+        </div>
+    </div>
+  </section>
+</section>
